0257794c9edb5da87b6e336cdfd4f3844b99a9f8
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-04-19 20:57:29 -0300

    feat: implement userProfile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { auth } from '../auth'
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const isLoggedIn = ref(auth.isLoggedIn())
+const currentUser = ref(auth.currentUser())
+const signOut = function () {
+    auth.signOut(() => (isLoggedIn.value = auth.isLoggedIn()))
+}
+</script>
+
+<template>
+    <main>
+        <template v-if="isLoggedIn">
+            <h3>Hi, {{ currentUser && currentUser.email }}</h3>
+            
+            <nav>
+                <a @click="signOut">Sign Out</a>
+            </nav>
+        </template>
+        <template v-else>
+            <h3>Please log in to have access</h3>
+            <nav>
+                <RouterLink :to="{ name: 'signin'}"> Sign In </RouterLink>
+            </nav>
+        </template>
+    </main>
+</template>
+
+<style scoped></style>
