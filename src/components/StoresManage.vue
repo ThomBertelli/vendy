40297fb59c2487b9886da3d8ac678aa5cf3ae4e2
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-05-23 20:46:24 -0300

    feat(StoresManage): add new stores button

--- src/components/StoresManage.vue
+++ src/components/StoresManage.vue
@@ -1,13 +1,30 @@
 <script lang="ts" setup>
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
     <div>
+    
+    
         <h1>Lojas</h1>
+    
+    <div>
+
+        <RouterLink :to="{name: 'new-store'}">
+            <ButtonPrime>Criar Loja</ButtonPrime>
+        
+        </RouterLink>
+
+
+
     </div>
+    
+    </div>
+
+
+
 </template>
 
-<style lang="postcss" scoped>
+<style scoped>
     
 </style>
