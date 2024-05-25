bc8d145dbf9cbd0b0dded3f88ec9bba330aa1389
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-05-24 21:18:31 -0300

    feat(StoresManage): show the user stores

--- src/components/StoresManage.vue
+++ src/components/StoresManage.vue
@@ -1,30 +1,53 @@
 <script lang="ts" setup>
 import { RouterLink } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const storesList = ref()
+
+const fetchProdutos = async () => {
+    try {
+        const response = await fetch(`${apiUrl}/stores`,{
+            headers: {
+                'Accept': 'application/json',
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+    }
+        }
+            
+        )
+        if (!response.ok) {
+            throw new Error('Erro ao buscar lojas')
+        }
+        const data = await response.json()
+        storesList.value = data
+        
+    } catch (error) {
+        console.error('Erro ao buscar lojas:', error)
+    }
+}
+
+onMounted(() => {fetchProdutos()})
 </script>
 
 <template>
     <div>
-    
-    
         <h1>Lojas</h1>
-    
-    <div>
 
-        <RouterLink :to="{name: 'new-store'}">
-            <ButtonPrime>Criar Loja</ButtonPrime>
-        
-        </RouterLink>
+        <div>
+            <RouterLink :to="{ name: 'new-store' }">
+                <ButtonPrime>Criar Loja</ButtonPrime>
+            </RouterLink>
+        </div>
 
-
-
+        <div>
+            <ul>
+                <li v-for="store in storesList" :key="store.id">
+                    {{ store.name }}
+                </li>
+            </ul>
+        </div>
     </div>
-    
-    </div>
-
-
-
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
