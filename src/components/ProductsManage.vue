e5409eca3bcba63e7ed93507d31f28d8e02959fc
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-05-26 00:59:50 -0300

    feat(ProductManage): add message when there are no products registered

--- src/components/ProductsManage.vue
+++ src/components/ProductsManage.vue
@@ -3,17 +3,24 @@ import { computed,ref,onMounted } from 'vue';
 import { useStore } from '../stores/useStore';
 import { useRouter } from 'vue-router';
 
+interface Product {
+  id: number;
+  name: string;
+  price: number
+}
+
+
 const apiUrl = import.meta.env.VITE_API_URL;
 const apiCredential = import.meta.env.VITE_API_CREDENTIAL
 const router = useRouter();
 const store = useStore();
-const storeId = computed(() => store.currentStore.id);
+const storeId = computed(() => store.currentStore?.id);
 const storeName = computed(() => store.currentStore.name);
-const productsList = ref()
+const productsList = ref<Product[]>([]);
 
 const fetchProducts = async () => {
 
-    console.log(storeId)
+
 
     try {
         const response = await fetch(`${apiUrl}/stores/${storeId.value}/products`, {
@@ -49,30 +56,33 @@ onMounted(() => { fetchProducts() })
 
         <div class="mt-20">
             <div class="flex flex-col items-center">
-                <ul v-if="productsList.value" class="flex flex-col gap-4 ">
-                <li v-for="product in productsList" 
-                    :key="product.id"
-                    
-                    class="items-center pt-2 pb-2 pl-6 pr-6 flex gap-20 rounded-md border border-amber-600 ">
-                    <h3  class="store-name flex-1 text-xl text-amber-600">
-                        {{ product.name }}
-                    </h3>
-                    <div class="flex gap-4">
-                        <i 
-                            class="pi pi-pen-to-square heartbeat cursor-pointer text-blue-500" style="font-size: 1.5rem"></i>
-                        <i class="pi pi-trash heartbeat cursor-pointer text-red-500" style="font-size: 1.5rem; color:red"></i>
-                    </div>
-                </li>
-            </ul>
+                <ul v-if="productsList.length" class="flex flex-col gap-4 ">
+                    <li v-for="product in productsList" 
+                        :key="product.id"
+                        
+                        class="items-center pt-2 pb-2 pl-6 pr-6 flex gap-20 rounded-md border border-amber-600 ">
+                            <h3  class="store-name flex-1 text-xl text-amber-600">
+                                {{ product.name }}
+                            </h3>
+                            <div class="flex gap-4">
+                                <i class="pi pi-pen-to-square heartbeat cursor-pointer text-blue-500" style="font-size: 1.5rem"></i>
+                                <i class="pi pi-trash heartbeat cursor-pointer text-red-500" style="font-size: 1.5rem; color:red"></i>
+                            </div>
+                    </li>
+                </ul>
 
-            <p v-else> Não há produtos nessa loja.</p>
+                <p v-else> Não há produtos nessa loja.</p>
 
             </div>
             
             
         </div>
 
-
+        <div class="fixed bottom-20 right-20">
+            <RouterLink :to="{ name: 'new-product' }">
+                <ButtonPrime>Cadastrar Produto</ButtonPrime>
+            </RouterLink>
+        </div>
 
     </div>
 </template>
